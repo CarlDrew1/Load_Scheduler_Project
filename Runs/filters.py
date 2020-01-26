5c0ad9d9e18d016aa4971e1401d3b821f4519c5e
Carl Drew <carldrew1@gmail.com>
2020-01-26 23:20:41 +1300

    searcg bar date picker

--- Runs/filters.py
+++ Runs/filters.py
@@ -1,10 +1,22 @@
 import django_filters
 from .models import Runs
+from django import forms
+
 
 
 
 
 class filter_runs(django_filters.FilterSet):
+    depart_date = django_filters.DateFilter(
+    lookup_expr='icontains',
+    widget=forms.DateInput(
+        attrs={
+            'id': 'date',
+            'type': 'text'
+        }
+    )
+    )
+
     class Meta:
         model = Runs
         fields = ['depart_date']
